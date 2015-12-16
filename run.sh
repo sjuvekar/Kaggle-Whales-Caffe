8b079af18a557252115144eae129464407ec758c
Sudeep Juvekar <sjuvekar@gmail.com>
2015-12-15 21:02:15 -0500

    run script

---
+++ run.sh
@@ -0,0 +1,4 @@
+$CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_1_x.prototxt -gpu 0
+$CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_1_y.prototxt -gpu 0
+$CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_2_x.prototxt -gpu 0
+$CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_2_y.prototxt -gpu 0
