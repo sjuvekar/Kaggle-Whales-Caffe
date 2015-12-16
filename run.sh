55f85a9cef72238ec9c897fc5d157ff8c7e82501
Sudeep Juvekar <sjuvekar@gmail.com>
2015-12-16 15:02:41 +0000

    Added classifier code

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
+# Train Deconv-networks for cropping
 $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_1_x.prototxt -gpu 0
 $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_1_y.prototxt -gpu 0
 $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_2_x.prototxt -gpu 0
 $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_2_y.prototxt -gpu 0
+
+# Train classifier on cropped training images
+$CAFFE_ROOT/build/tools/caffe train -solver solvers/classifier_solver.prototxt -gpu 0
