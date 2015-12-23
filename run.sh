3a10d150ee8ce9b8c30af3877d0a48dd5cbaa384
opraveen <opraveen@gmail.com>
2015-12-23 00:15:59 -0800

    prottxt for two localizer networks

--- run.sh
+++ run.sh
@@ -14,6 +14,11 @@ $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_1_y.prototxt -gpu 0
 $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_2_x.prototxt -gpu 0
 $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_2_y.prototxt -gpu 0
 
+## Uncomment these to use two networks for each of the points
+# $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_1.prototxt -gpu 0
+# $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_2.prototxt -gpu 0
+### TODO: Add code (python?) to rotate&crop test images(testcrops) using estimated points1/2
+
 # Train classifier on cropped training images
 $CAFFE_ROOT/build/tools/caffe train -solver solvers/classifier_solver.prototxt -gpu 0
 
