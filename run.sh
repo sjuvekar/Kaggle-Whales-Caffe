868fd26c55a6bdb43c876da1a21ac30d202dc8e1
Sudeep Juvekar <sjuvekar@gmail.com>
2015-12-23 19:43:34 +0000

    Adding predictions to run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,6 @@ $CAFFE_ROOT/build/tools/caffe train -solver solvers/solver_2_y.prototxt -gpu 0
 
 # Train classifier on cropped training images
 $CAFFE_ROOT/build/tools/caffe train -solver solvers/classifier_solver.prototxt -gpu 0
+
+# Predict and create submission csv file
+python submit.py
